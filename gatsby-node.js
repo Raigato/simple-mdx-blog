98f7e6ddca479d1471530a42a3f16bc1834241ff
RoiGoujat <whysoserious_06@live.fr>
2020-07-03 20:08:56 +0200

    Added allPosts template

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,45 +1,45 @@
-// exports.createPages = async function ({ actions, graphql }) {
-//   const { data } = await graphql`
-//     query {
-//       allMdx(sort: { fields: frontmatter___date, order: DESC }) {
-//         edges {
-//           node {
-//             frontmatter {
-//               slug
-//             }
-//             id
-//           }
-//         }
-//       }
-//     }
-//   `
+exports.createPages = async function ({ actions, graphql }) {
+  const { data } = await graphql(`
+    query {
+      allMdx(sort: { fields: frontmatter___date, order: DESC }) {
+        edges {
+          node {
+            frontmatter {
+              slug
+            }
+            id
+          }
+        }
+      }
+    }
+  `)
 
-//   // Created paginated pages for posts
-//   const postPerPage = 3
+  // Created paginated pages for posts
+  const postPerPage = 3
 
-//   const numPages = Math.ceil(data.allMdx.edges.length / postPerPage)
+  const numPages = Math.ceil(data.allMdx.edges.length / postPerPage)
 
-//   Array.from({ length: numPages }).forEach((_, index) => {
-//     actions.createPages({
-//       path: index === 0 ? `/` : `/${index + 1}`,
-//       component: require.resolve(`./src/templates/allPosts.js`),
-//       context: {
-//         limit: postPerPage,
-//         skip: index * postPerPage,
-//         numPages,
-//         currentPage: index + 1,
-//       },
-//     })
-//   })
+  Array.from({ length: numPages }).forEach((_, index) => {
+    actions.createPage({
+      path: index === 0 ? `/` : `/${index + 1}`,
+      component: require.resolve(`./src/templates/allPosts.js`),
+      context: {
+        limit: postPerPage,
+        skip: index * postPerPage,
+        numPages,
+        currentPage: index + 1,
+      },
+    })
+  })
 
-//   // Create signle blog posts
-//   data.allMdx.edges.forEach(edge => {
-//     const slug = edge.node.frontmatter.slug
-//     const id = edge.node.id
-//     actions.createPages({
-//       path: slug,
-//       component: require.resolve(`./src/templates/singlePost.js`),
-//       context: { id },
-//     })
-//   })
-// }
+  // // Create signle blog posts
+  // data.allMdx.edges.forEach(edge => {
+  //   const slug = edge.node.frontmatter.slug
+  //   const id = edge.node.id
+  //   actions.createPages({
+  //     path: slug,
+  //     component: require.resolve(`./src/templates/singlePost.js`),
+  //     context: { id },
+  //   })
+  // })
+}
