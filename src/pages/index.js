fca896202f287f620a335bb98d9961e754fd8a66
RoiGoujat <whysoserious_06@live.fr>
2020-07-03 19:11:00 +0200

    Added Content Cards

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,18 @@
 import React from "react"
-import { Container, FeaturedImage } from "../components"
+import { Container, FeaturedImage, Content, Card } from "../components"
 
 const IndexPage = () => {
   return (
     <Container>
       <FeaturedImage />
+      <Content>
+        <Card
+          date="July 3, 2020"
+          title="Lorem ipsum dolor sit amet."
+          excerpt="Lorem ipsum dolor sit amet consectetur adipisicing elit. Sed hic excepturi, veritatis quam labore fuga nihil nisi optio in earum repudiandae quibusdam mollitia vel ea quasi ipsa eum saepe dolorum, quod voluptatum iste illo ab. Illum asperiores vero dolorum deserunt ipsam, alias soluta molestias repellendus adipisci fugit unde maiores ad nostrum quod eveniet numquam distinctio esse? Voluptates error odit id."
+          slug="/test"
+        />
+      </Content>
     </Container>
   )
 }
